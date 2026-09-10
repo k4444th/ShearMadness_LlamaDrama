7bbd40197dc4143f947ebb9d35a48a6fe5417fe9
Katharina Weinberger <kath.weinberger@icloud.com>
2026-09-10 19:42:07 +0200

    Basic Llama movement

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ShearMadness_LlamaDrama
-
---
+++ scripts/llama.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+# Movement variables
+var direction := Vector2.ZERO
+var initialSpeed := 25.0
+var maxSpeed := 75
+var speed := initialSpeed
+
+# Nodes (onready)
+@onready var sprite := $Sprite
+
+func _process(_delta: float) -> void:
+	getKeyboardInputs()
+	setAnimation()
+	moveSprite()
+
+func getKeyboardInputs():
+	direction.x = Input.get_axis("Left", "Right")
+	direction.y = Input.get_axis("Up", "Down")
+	direction = direction.normalized()
+
+func setAnimation():
+	var animationString := "walk_" if direction else "idle_"
+	
+	if direction.x != 0:
+		animationString += "side"
+		
+		if direction.x < 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
+	
+	elif direction.y < 0:
+		animationString += "back"
+	
+	else:
+		animationString += "front"
+	
+	sprite.play(animationString)
+	print(animationString)
+
+func moveSprite():
+	velocity = direction * speed
+	move_and_slide()
+	if speed < maxSpeed:
+			speed *= 1.025
